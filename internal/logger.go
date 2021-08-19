9c60cf0cd561e93aef2f497460bbe084b56f6805
houser <houser@houserqu.com>
2021-08-20 00:14:29 +0800

    feat: log file 优化

--- internal/logger.go
+++ internal/logger.go
@@ -1,21 +1,35 @@
 package internal
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var defaultLogger *log.Logger
 
 func init() {
-	writer, err := os.OpenFile("./logs/default.log", os.O_WRONLY|os.O_CREATE, 0755)
+	// 获取日志目录绝对路径
+	logFolderPath, err := filepath.Abs(os.Getenv("LOG_PATH"))
 	if err != nil {
-		log.Fatalf("create log file failed: %v", err)
+		log.Fatalf(err.Error())
 	}
 
-	fmt.Println("init log")
+	// 判断目录是否存在，不存在则创建目录
+	_, err = os.Stat(logFolderPath)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(logFolderPath, os.ModePerm)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+	}
+
+	// 打开日志文件
+	writer, err := os.OpenFile(filepath.Join(logFolderPath, "default.log"), os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 
 	defaultLogger = log.New(io.MultiWriter(writer), "", log.Lshortfile|log.LstdFlags)
 }
